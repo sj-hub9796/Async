d74097aefe1f0def0c0126dad7dc44e75faf6efa
axalotl <ladyginnic815@gmail.com>
2024-12-11 21:18:03 +0500

    Update sync

--- src/main/java/com/axalotl/async/mixin/utils/SyncAllMixin.java
+++ src/main/java/com/axalotl/async/mixin/utils/SyncAllMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.advancement.criterion.AbstractCriterion;
 import net.minecraft.entity.ai.WardenAngerManager;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.PathMinHeap;
+import net.minecraft.util.collection.TypeFilterableList;
 import net.minecraft.util.math.random.CheckedRandom;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.chunk.light.LevelPropagator;
@@ -23,7 +24,8 @@ import org.spongepowered.asm.mixin.Mixin;
         AbstractCriterion.class,
         WardenAngerManager.class,
         WorldBorder.class,
-        EntityTrackingSection.class
+        EntityTrackingSection.class,
+        TypeFilterableList.class
 })
 public class SyncAllMixin {
 }
