c962aa7da8cbe9bac756429e29b77b5c1a2f0116
axalotl <ladyginnic815@gmail.com>
2024-12-23 17:20:17 +0500

    Fix movement

--- src/main/java/com/axalotl/async/mixin/entity/ServerEntityManagerListenerMixin.java
+++ src/main/java/com/axalotl/async/mixin/entity/ServerEntityManagerListenerMixin.java
@@ -5,26 +5,17 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerEntityManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-
-import java.util.concurrent.locks.ReentrantLock;
 
 @Mixin(ServerEntityManager.Listener.class)
 public abstract class ServerEntityManagerListenerMixin implements AutoCloseable {
-    @Unique
-    private static final ReentrantLock lock = new ReentrantLock();
 
     @WrapMethod(method = "updateEntityPosition")
-    private void updateEntityPosition(Operation<Void> original) {
-        synchronized (lock) {
-            original.call();
-        }
+    private synchronized void updateEntityPosition(Operation<Void> original) {
+        original.call();
     }
 
     @WrapMethod(method = "remove")
-    private void remove(Entity.RemovalReason reason, Operation<Void> original) {
-        synchronized (lock) {
-            original.call(reason);
-        }
+    private synchronized void remove(Entity.RemovalReason reason, Operation<Void> original) {
+        original.call(reason);
     }
 }
