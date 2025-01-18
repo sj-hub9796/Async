0ee6769b1142cb340cc2117c4e088050cb72425c
AxalotL <ladyginnic815@gmail.com>
2025-01-18 22:17:03 +0500

    Update

--- src/main/java/com/axalotl/async/mixin/entity/LivingEntityMixin.java
+++ src/main/java/com/axalotl/async/mixin/entity/LivingEntityMixin.java
@@ -8,11 +8,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = LivingEntity.class, priority = 1001)
 public abstract class LivingEntityMixin extends Entity {
@@ -34,11 +30,5 @@ public abstract class LivingEntityMixin extends Entity {
     @WrapMethod(method = "tickEffects")
     private synchronized void tickStatusEffects(Operation<Void> original) {
         original.call();
-    }
-
-    @Inject(method = "onClimbable", at = @At("HEAD"), cancellable = true)
-    private void onClimbable(CallbackInfoReturnable<Boolean> cir) {
-        BlockState blockState = this.getInBlockState();
-        if (blockState == null) cir.setReturnValue(false);
     }
 }
