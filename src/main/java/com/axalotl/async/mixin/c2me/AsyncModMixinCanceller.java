d753953cdc7f0af99823419fbbf5dd1c3235c912
AxalotLDev <ladyginnic815@gmail.com>
2025-01-21 15:11:31 +0500

    Cancel com.ishland.c2me.fixes.general.threading_issues.mixin.threading_detections.random_instances.MixinWorld

--- src/main/java/com/axalotl/async/mixin/c2me/AsyncModMixinCanceller.java
+++ src/main/java/com/axalotl/async/mixin/c2me/AsyncModMixinCanceller.java
@@ -9,7 +9,8 @@ public class AsyncModMixinCanceller implements MixinCanceller {
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
         if (FabricLoader.getInstance().isModLoaded("c2me")) {
-            return mixinClassName.equals("com.ishland.c2me.fixes.general.threading_issues.mixin.asynccatchers.MixinThreadedAnvilChunkStorage");
+            return mixinClassName.equals("com.ishland.c2me.fixes.general.threading_issues.mixin.asynccatchers.MixinThreadedAnvilChunkStorage") ||
+                    mixinClassName.equals("com.ishland.c2me.fixes.general.threading_issues.mixin.threading_detections.random_instances.MixinWorld");
         } else return false;
     }
 }
