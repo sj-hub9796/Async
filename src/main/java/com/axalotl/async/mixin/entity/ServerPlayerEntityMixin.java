581404641989e74e0786ec0a6e1c18abb61ce73b
AxalotLDev <ladyginnic815@gmail.com>
2025-01-21 15:32:10 +0500

    Try fix polymer mod

--- src/main/java/com/axalotl/async/mixin/entity/ServerPlayerEntityMixin.java
+++ src/main/java/com/axalotl/async/mixin/entity/ServerPlayerEntityMixin.java
@@ -9,9 +9,15 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+    @Unique
+    private static final ReentrantLock lock = new ReentrantLock();
+
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
@@ -19,5 +25,12 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     @WrapMethod(method = "onDeath")
     private synchronized void onDead(DamageSource damageSource, Operation<Void> original) {
         original.call(damageSource);
+    }
+
+    @WrapMethod(method = "onDisconnect")
+    private void onDisconnect(Operation<Void> original) {
+        synchronized (lock) {
+            original.call();
+        }
     }
 }
