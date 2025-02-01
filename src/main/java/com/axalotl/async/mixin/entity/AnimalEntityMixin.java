8865e4e2d5716517da9ee38bfa441e4e8b639595
AxalotL <ladyginnic815@gmail.com>
2025-02-01 15:49:04 +0500

    Full fix animal breed

--- src/main/java/com/axalotl/async/mixin/entity/AnimalEntityMixin.java
+++ src/main/java/com/axalotl/async/mixin/entity/AnimalEntityMixin.java
@@ -2,21 +2,51 @@ package com.axalotl.async.mixin.entity;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 @Mixin(AnimalEntity.class)
-public class AnimalEntityMixin {
-    @Unique
-    private static final ReentrantLock lock = new ReentrantLock();
+public abstract class AnimalEntityMixin extends Entity {
 
-    @WrapMethod(method = "canBreedWith")
-    private boolean canBreedWith(AnimalEntity other, Operation<Boolean> original) {
-        synchronized (lock) {
-            return original.call(other);
+    public AnimalEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    /**
+     * Utility method to acquire a consistent lock ordering for two animals.
+     */
+    @Unique
+    private static Object[] getOrderedLocks(AnimalEntity a, AnimalEntity b) {
+        if (System.identityHashCode(a) <= System.identityHashCode(b)) {
+            return new Object[]{a, b};
+        } else {
+            return new Object[]{b, a};
+        }
+    }
+
+    @WrapMethod(method = "breed(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/AnimalEntity;Lnet/minecraft/entity/passive/PassiveEntity;)V")
+    private void breed(ServerWorld world, AnimalEntity other, PassiveEntity baby, Operation<Void> original) {
+        Object[] locks = getOrderedLocks((AnimalEntity)(Object)this, other);
+        synchronized (locks[0]) {
+            synchronized (locks[1]) {
+                original.call(world, other, baby);
+            }
+        }
+    }
+
+    @WrapMethod(method = "breed(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/AnimalEntity;)V")
+    private void breed(ServerWorld world, AnimalEntity other, Operation<Void> original) {
+        Object[] locks = getOrderedLocks((AnimalEntity)(Object)this, other);
+        synchronized (locks[0]) {
+            synchronized (locks[1]) {
+                original.call(world, other);
+            }
         }
     }
 }
